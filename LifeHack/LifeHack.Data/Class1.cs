46e47e852592e99ebbba7805f4b5d3fea235451e
Moises Goncalves <moises@bookchoice.com>
2020-06-25 19:00:35 +0200

    Initial Commit

---
+++ LifeHack/LifeHack.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LifeHack.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ LifeHack/LifeHack.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LifeHack.Domain
+{
+    public class Class1
+    {
+    }
+}
